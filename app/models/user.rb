9e1c181cb53acae4115d4da01a98fa23b8485870
karenakimoto <karen_akimoto@icloud.com>
2020-09-21 01:16:30 +0900

    userモデルの導入

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email: format:{ with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :password, format:{ with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
+    validates :familyname, format:{ with:/\A[ぁ-んァ-ンー-龥]/}
+    validates :firstname, format:{ with:/\A[ぁ-んァ-ンー-龥]/}
+    validates :familyname_kana, format:{ with: /\A[ァ-ヶー－]+\z/}
+    validates :firstname_kana, format:{ with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
+
+end
