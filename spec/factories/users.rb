a546b489e1e150addf49c86a604d04fd3903acf0
karenakimoto <karen_akimoto@icloud.com>
2020-09-21 02:16:30 +0900

    rubocopの実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"karen"}
-    email                 {"kkk@gmail.com"}
-    password              {"kare0000"}
-    password_confirmation {password}
-    familyname            {"秋本"}
-    firstname             {"華蓮"}
-    familyname_kana       {"アキモト"}
-    firstname_kana        {"カレン"} 
-    birthday              {20000130}
+    nickname              { 'karen' }
+    email                 { 'kkk@gmail.com' }
+    password              { 'kare0000' }
+    password_confirmation { password }
+    familyname            { '秋本' }
+    firstname             { '華蓮' }
+    familyname_kana       { 'アキモト' }
+    firstname_kana        { 'カレン' }
+    birthday              { 20_000_130 }
   end
 end
