cb299bcffeca1ac52b434d7bfe2867286976639b
karenakimoto <karen_akimoto@icloud.com>
2020-09-21 20:32:10 +0900

    rubocopの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,12 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :email, format:{ with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-    validates :password, format:{ with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i}
-    validates :familyname, format:{ with:/\A[ぁ-んァ-ンー-龥]/}
-    validates :firstname, format:{ with:/\A[ぁ-んァ-ンー-龥]/}
-    validates :familyname_kana, format:{ with: /\A[ァ-ヶー－]+\z/}
-    validates :firstname_kana, format:{ with: /\A[ァ-ヶー－]+\z/}
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
+    validates :familyname, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
+    validates :firstname, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
+    validates :familyname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
-
 end
