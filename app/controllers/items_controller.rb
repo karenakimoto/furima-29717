008a4800b16ed7c171f53d189b6a87dae98a4a64
karenakimoto <karen_akimoto@icloud.com>
2020-09-25 14:29:09 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: :new 
   before_action :set_item, only: [:show, :destroy, :edit, :update]
 
   def index
@@ -31,6 +31,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless user_signed_in? && current_user.id == @item.user_id
+      render :show
+    end
   end
 
   def update
