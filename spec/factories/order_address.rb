044e0719952fd3e3fdadf8367571eef4fbb4e3e4
karenakimoto <karen_akimoto@icloud.com>
2020-09-30 11:56:43 +0900

    rubocopによる修正

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     postal_code      { '111-1111' }
     prefectures_id   { 2 }
     city             { '横浜市' }
-    address          {'青山1-1'}
+    address          { '青山1-1'}
     building         {'8-8'}
     phonenumber      {'09088888888'}
     token            {'n1n1n1n1n1n1n1n1n1n1n'}
